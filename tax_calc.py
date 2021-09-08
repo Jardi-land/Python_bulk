1b2c6edc8c31c90cceca4c31e045190fcd6f19bf
Jardi-land <Lorenzodezen@outlook.com>
2021-09-08 23:05:58 +0200

    Changed tax_clac

--- tax_calc.py
+++ tax_calc.py
@@ -1,11 +1,9 @@
-from colors import bcolors
-
-nombre = input(f"Entrer le montant: {bcolors.OKBLUE}")
+nombre = input(f"Entrer le montant: ")
 if not nombre.isdigit():
 #if type(nombre) != int or float:
-    print(f"{bcolors.ENDC}Merci d'entrer un nombre")
+    print("Merci d'entrer un nombre")
 else:
     nombre = float(nombre)
     nombre_4 = nombre - nombre/100*4
     nombre_2 = nombre_4 - nombre_4/100*2
-    print(f"{bcolors.ENDC}Montant restant après les taxes: {bcolors.OKBLUE + str(nombre_2) + bcolors.ENDC}")
+    print(f"Montant restant après les taxes: {str(nombre_2)}")
