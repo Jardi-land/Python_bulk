d8a6d31327d2fd8ee7efa2826346373b32fa227f
Jardi-land <Lorenzodezen@outlook.com>
2021-09-09 23:28:43 +0200

    Added game_over, win and ask_replay

--- pendu/fc_interface.py
+++ pendu/fc_interface.py
@@ -32,3 +32,55 @@ def update_interface(word, custom, string, no_letter, hangphase):
     else:
         test_letter = input(">> Tester une lettre: ")
     return test_letter
+
+def ask_replay(word, hangphase, no_letter, colors, function):
+    replay = input(f"{colors}Voulez-vous rejouer ? [1] Oui [2] Non:{bcolors.ENDC}")
+    if replay.isdigit():
+        replay = int(replay)
+        if replay < 1:
+            if function == "win":
+                return win(word, hangphase, no_letter)
+            else:
+                return game_over(word, hangphase, no_letter)
+        elif replay > 2:
+            if function == "win":
+                return win(word, hangphase, no_letter)
+            else:
+                return game_over(word, hangphase, no_letter)
+        elif replay == 1:
+            return True
+        elif replay == 2:
+            return False
+    else:
+        if function == "win":
+            return win(word, hangphase, no_letter)
+        else:
+            return game_over(word, hangphase, no_letter)
+
+def game_over(word, hangphase, no_letter):
+    clear_all()
+    lettre_devi_str = ""
+    word_str = ""
+    for i in word:
+        word_str = f"{word_str}{i}"
+    print(f"{bcolors.FAIL}VOUS AVEZ PERDU !{bcolors.ENDC}")
+    print(separator)
+    print(f"{bcolors.FAIL}{hang[hangphase]}{bcolors.ENDC}")
+    print(separator)
+    print(f"{bcolors.FAIL}Le mot était: {word_str}{bcolors.ENDC}")
+    print(separator)
+    return ask_replay(word, hangphase, no_letter, bcolors.FAIL, "game_over")
+
+def win(word, hangphase, no_letter):
+    clear_all()
+    lettre_devi_str = ""
+    word_str = ""
+    for i in word:
+        word_str = f"{word_str}{i}"
+    print(f"{bcolors.OKGREEN}VOUS AVEZ GAGNÉ !{bcolors.ENDC}")
+    print(separator)
+    print(f"{bcolors.OKGREEN}{hang[hangphase]}{bcolors.ENDC}")
+    print(separator)
+    print(f"{bcolors.OKGREEN}Le mot était: {word_str}{bcolors.ENDC}")
+    print(separator)
+    return ask_replay(word, hangphase, no_letter, bcolors.OKGREEN, "win")
