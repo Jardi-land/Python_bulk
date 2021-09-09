f754493d9c91c3e7af0e1d4427c6953159b661bd
Jardi-land <Lorenzodezen@outlook.com>
2021-09-09 16:12:41 +0200

    Added Letter devi and  letter good str

--- pendu/fc_interface.py
+++ pendu/fc_interface.py
@@ -5,15 +5,21 @@ import platform
 def clear_all():
     os.system(f'cmd /c {os_cmd[os_cmd.index(platform.system()) + 1]}')
 
-def lettre_devi_fc(letter):
-    lettre_devi.append(letter)
-    update_interface
+def lettre_devi_fc(letter, word):
+    try:
+        word.index(letter)
+        return True
+    except:
+        return False
 
 def update_interface(word, custom, string):
     clear_all()
     lettre_devi_str = ""
+    lettre_good_str = ""
     for i in lettre_devi:
         lettre_devi_str = f"{i} {lettre_devi_str}"
+    for i in lettre_good:
+        lettre_good_str = f"{i} {lettre_good}"
     print(f"Lettre(s) en trop: {lettre_devi_str}{no_letter * (9 - len(lettre_devi))}")
     print(separator)
     print(hang[hangphase])
