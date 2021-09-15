b22fd0e870ce84ec86b354649208fd5dbbd66500
Jardi-land <Lorenzodezen@outlook.com>
2021-09-15 14:48:25 +0200

    Added ask_number()

--- task_python/01/main.py
+++ task_python/01/main.py
@@ -7,4 +7,14 @@ os_cmd = ["Windows", "cls", "macOS", "clear", "Linux", "clear"]
 def clear_all():
     os.system(f"{os_cmd[os_cmd.index(platform.system()) + 1]}")
 
+def ask_number():
+    a = input("Ton nombre: ")
+    if not a.isdigit():
+        clear_all()
+        print(f"{bcolors.FAIL}Merci de mettre un nombre !")
+        ask_number()
+    else:
+        calc(a)
+
 clear_all()
+ask_number()
